cbb133658291dcf1c4ac53e38c41f97c86a8b0e8
Marco Re <marco.re@polimi.it>
2015-10-01 14:45:08 +0200

    Better default settings for local django runserver

--- settings.py
+++ settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oai',
-    'xapi'
+    'oai'
 )
 
 MIDDLEWARE_CLASSES = (
