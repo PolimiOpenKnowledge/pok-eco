3f6759d8b2bb28e236162eaddf9d34eb21615e14
Marco Re <marco.re@polimi.it>
2015-10-30 11:32:02 +0100

    Alphabetic sorted

--- xapi/patterns/__init__.py
+++ xapi/patterns/__init__.py
@@ -1,12 +1,12 @@
 from xapi.patterns.access_course import AccessCourseRule
 from xapi.patterns.access_module import AccessModuleRule
 from xapi.patterns.access_problem import AccessProblemRule
-from xapi.patterns.problem_check import ProblemCheckRule
+from xapi.patterns.access_wiki import AccessWikiRule, AccessWikiPageRule
+from xapi.patterns.base import BasePattern
 from xapi.patterns.enrollment import (
     LearnerUnEnrollMOOCRule,
     LearnerEnrollMOOCRule
 )
 from xapi.patterns.manage_wiki import CreateWikiRule, EditWikiRule
-from xapi.patterns.access_wiki import AccessWikiRule, AccessWikiPageRule
+from xapi.patterns.problem_check import ProblemCheckRule
 from xapi.patterns.video import LoadVideoRule, PlayVideoRule
-from xapi.patterns.base import BasePattern
