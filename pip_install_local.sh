8fa32cfd4b61eda0a9637044fa783c930daa5a97
Marco Re <marco.re@polimi.it>
2015-12-11 00:38:07 +0100

    Revert order in requirements

--- pip_install_local.sh
+++ pip_install_local.sh
@@ -4,5 +4,5 @@ easy_install pip
 pip install coveralls
 pip install pep8
 pip install pylint
-pip install --exists-action w -r requirements.txt
-pip install --exists-action w -r test-requirements.txt
+pip install -r requirements.txt
+pip install -r test-requirements.txt
