c3473795b97963afa026f92abbc631c5c69a46c8
Marco Re <marco.re@polimi.it>
2015-12-10 15:45:49 +0100

    Force pip wipe

--- pip_install_local.sh
+++ pip_install_local.sh
@@ -5,4 +5,4 @@ pip install coveralls
 pip install pep8
 pip install pylint
 pip install -r requirements.txt
-pip install -r test-requirements.txt
+pip install --exists-action w  -r test-requirements.txt
