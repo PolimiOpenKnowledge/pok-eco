2df36e6380534e594996064a416060bef73d2877
Marco Re <marco.re@polimi.it>
2015-12-11 01:10:49 +0100

    Upgrade pylint

--- pip_install_local.sh
+++ pip_install_local.sh
@@ -3,6 +3,6 @@ set -ex
 easy_install pip
 pip install coveralls
 pip install pep8
-pip install pylint
+pip install --upgrade pylint==1.5.1
 pip install -r requirements.txt
 pip install -r test-requirements.txt
