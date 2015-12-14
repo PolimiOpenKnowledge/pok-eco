da6ded136506943419f9e6aae4bbd7f69c3c4506
Marco Re <marco.re@polimi.it>
2015-12-14 15:09:46 +0100

    Revert to module separated pep8 evaluation

--- run_tests.sh
+++ run_tests.sh
@@ -10,7 +10,9 @@ coverage run --source=ecoapi ./manage.py test ecoapi
 coverage report -m
 coverage run --source=oai ./manage.py test oai
 coverage report -m
-pep8 --config=.pep8 --exclude=track,edxmako,course_modes,util,static_replace,external_auth,third_party_auth,student,xblock_django,microsite_configuration,certificates,branding,courseware,psychometrics,verify_student
+pep8 --config=.pep8 ecoapi
+pep8 --config=.pep8 xapi
+pep8 --config=.pep8 oai
 pylint --rcfile=.pylintrc ecoapi --report=no
 pylint --rcfile=.pylintrc xapi --report=no
 pylint --rcfile=.pylintrc oai --report=no
