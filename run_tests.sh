825cbfaa386848e70e31498c990f02d99d96359f
Marco Re <marco.re@polimi.it>
2015-12-14 14:58:44 +0100

    Exclude edx dependencies installed app from pep8

--- run_tests.sh
+++ run_tests.sh
@@ -10,7 +10,7 @@ coverage run --source=ecoapi ./manage.py test ecoapi
 coverage report -m
 coverage run --source=oai ./manage.py test oai
 coverage report -m
-pep8 --config=.pep8
+pep8 --config=.pep8 --exclude=track,edxmako,course_modes,util,static_replace,external_auth,third_party_auth,student,xblock_django,microsite_configuration,certificates,branding,courseware,psychometrics,verify_student
 pylint --rcfile=.pylintrc ecoapi --report=no
 pylint --rcfile=.pylintrc xapi --report=no
 pylint --rcfile=.pylintrc oai --report=no
