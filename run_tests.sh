6abbd036e68acc7771011afafdded222cc3ffdac
Marco Re <marco.re@polimi.it>
2015-12-14 14:48:12 +0100

    Unified pep8 elaboration

--- run_tests.sh
+++ run_tests.sh
@@ -10,9 +10,7 @@ coverage run --source=ecoapi ./manage.py test ecoapi
 coverage report -m
 coverage run --source=oai ./manage.py test oai
 coverage report -m
-pep8 --config=.pep8 oai
+pep8 --config=.pep8
 pylint --rcfile=.pylintrc oai --report=no
-pep8 --config=.pep8 ecoapi
 pylint --rcfile=.pylintrc ecoapi --report=no
-pep8 --config=.pep8 xapi
 pylint --rcfile=.pylintrc xapi --report=no
