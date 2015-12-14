184363d22e915930608f14604b86b076f5d8285e
Marco Re <marco.re@polimi.it>
2015-12-14 19:34:33 +0100

    Fix id of object in access course

--- xapi/patterns/access_course.py
+++ xapi/patterns/access_course.py
@@ -18,7 +18,7 @@ class AccessCourseRule(BasePattern, LearnerAccessesMoocVerb):
     def convert(self, evt, course_id):
         verb = self.get_verb()
         obj = Activity(
-            id=self.fix_id(self.base_url, evt['event_type']),
+            id=self.oai_prefix + course_id,  # self.fix_id(self.base_url, evt['event_type'])
             definition=ActivityDefinition(
                 name=LanguageMap({'en-US': self.oai_prefix + course_id}),
                 type="http://adlnet.gov/expapi/activities/course"
