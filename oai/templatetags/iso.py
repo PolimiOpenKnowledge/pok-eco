d5ca11bb58e9e48cbae7fa62096b6496a9c5410d
Marco Re <marco.re@polimi.it>
2015-12-22 21:34:32 +0100

    Fix register case

--- oai/templatetags/iso.py
+++ oai/templatetags/iso.py
@@ -5,10 +5,10 @@ from django import template
 from django.utils.safestring import mark_safe
 from django.utils.timezone import make_naive, UTC
 
-REGISTER = template.Library()
+register = template.Library()
 
 
-@REGISTER.filter(is_safe=True)
-@REGISTER.simple_tag(name="tisoformat")
+@register.filter(is_safe=True)
+@register.simple_tag(name="tisoformat")
 def tisoformat(date):
     return mark_safe(make_naive(date, UTC()).replace(microsecond=0).isoformat()+'Z')
