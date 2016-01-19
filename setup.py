239bb04ad908f0f51e970500937f14a37048e0ed
Marco Re <marco.re@polimi.it>
2016-01-19 19:53:14 +0100

    Tag version 0.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pok-eco',
-    version='0.1.6',
+    version='0.1.8',
     description='POK-ECO Integrations ',
     author='METID - Politecnico di Milano',
     url='http://www.metid.polimi.it',
