15e37c8b55adc28b37d3fee52686dc265da0d44a
Marco Re <marco.re@polimi.it>
2016-02-05 16:19:28 +0100

    Import fix

--- ecoapi/urls.py
+++ ecoapi/urls.py
@@ -11,5 +11,5 @@ urlpatterns = patterns(
     url(r'^users/(?P<eco_user_id>[^/]+)/courses', 'ecoapi.views.user_courses', name='ecoapi_user_courses'),
     url(r'^teachers/(?P<id_teacher>[^/]+)', 'ecoapi.views.teacher_view', name='ecoapi_teacher'),
     url(r'^heartbeat', 'ecoapi.views.heartbeat', name='ecoapi_heartbeat'),
-    url(r'^tasks/{course_key}$'.format(settings.COURSE_ID_PATTERN), 'ecoapi.views.tasks', name='ecoapi_tasks'),
+    url(r'^tasks/{}$'.format(settings.COURSE_ID_PATTERN), 'ecoapi.views.tasks', name='ecoapi_tasks'),
 )
