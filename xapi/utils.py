d24a1540317ac531d5bdcb020f507f776370b964
Marco Re <marco.re@polimi.it>
2016-12-02 17:33:34 +0100

    Remove dependency from courseware.course and get_course_by_id

--- xapi/utils.py
+++ xapi/utils.py
@@ -8,8 +8,6 @@ from xmodule.modulestore.django import modulestore
 from django.contrib.auth.models import User
 from django.test.client import RequestFactory
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from courseware.courses import get_course_by_id
-from courseware.module_render import get_module
 
 
 def get_request_for_user(user):
@@ -32,13 +30,6 @@ def get_course_key(course_id):
         course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
     return course_key
 
-
-def get_course(course_id):
-    course_key = get_course_key(course_id)
-    course = get_course_by_id(course_key)
-    return course
-
-
 def get_course_title(course_id):
     course_key = get_course_key(course_id)
     title = CourseOverview.get_from_id(course_key).display_name
@@ -46,14 +37,9 @@ def get_course_title(course_id):
 
 
 def get_course_description(course_id, user_id):
-    course = get_course(course_id)
-    user = User.objects.get(id=user_id)
-    request = get_request_for_user(user)
-    module = get_module(user, request, course.location.replace(category='about', name="short_description"), [])
-    if module:
-        return module.data
-    else:
-        return ""
+    course_key = get_course_key(course_id)
+    description = CourseOverview.get_from_id(course_key).short_description
+    return description
 
 
 def get_usage_key(course_id, module_id):
